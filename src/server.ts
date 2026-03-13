8f6a199f01d565d66440a1fc409a9cc4dbe72dac
Rito Ramadhan <126365429+mas-rito@users.noreply.github.com>
2026-03-13 07:31:02 +0000

    Latihan routing products dan implementasi method POST

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,30 @@
+const products = [
+    { id: 1, name: "Laptop", price: 15000000 },
+    { id: 2, name: "Mouse", price: 250000 },
+    { id: 3, name: "Keyboard", price: 750000 }
+];
+
 const server = Bun.serve({
     port: 3000,
     fetch(request) {
         const url = new URL(request.url);
         const path = url.pathname;
         const method = request.method;
+        const pathSegments = path.split("/");
         
         console.log(`[${new Date().toLocaleTimeString()}] ${method} ${path}`);
 
         if (path === '/' && method === 'GET') {
-            return new Response('<h1>🏠 Halaman Utama (Bun)</h1><p>Selamat datang di server Bun + TypeScript!</p>', {
+            return new Response('<h1>Halaman Utama (Bun)</h1><p>Selamat datang di server Bun + TypeScript!</p>', {
                 headers: { 'Content-Type': 'text/html' },
             });
         }
         else if (path === '/about' && method === 'GET') {
-            return new Response(`<h1>📄 Tentang Kami (Bun)</h1><p>Routing manual dengan Bun sangat mudah! </p>`, {
+            return new Response(`<h1>Tentang Kami (Bun)</h1><p>Routing manual dengan Bun sangat mudah! </p>`, {
                 headers: { 'Content-Type': 'text/html' },
             });
         }
+        
         else if (path === '/api/users' && method === 'GET') {
             return new Response(JSON.stringify([
                 { id: 1, name: 'Alice' },
@@ -33,8 +41,31 @@ const server = Bun.serve({
                 headers: { 'Content-Type': 'application/json' },
             });
         }
+        
+        if (path === '/products' && method === 'GET') {
+            return new Response(JSON.stringify(products), {
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+
+        if (pathSegments[1] === 'products' && pathSegments[2] && method === 'GET') {
+            const id = parseInt(pathSegments[2]);
+            const product = products.find(p => p.id === id);
+
+            if (product) {
+                return new Response(JSON.stringify(product), {
+                    headers: { 'Content-Type': 'application/json' },
+                });
+            } else {
+                return new Response(JSON.stringify({ message: "Produk tidak ditemukan" }), {
+                    status: 404,
+                    headers: { 'Content-Type': 'application/json' },
+                });
+            }
+        }
+
         else {
-            return new Response('<h1>❌ 404 - Halaman Tidak Ditemukan (Bun)</h1>', {
+            return new Response('<h1>404 - Halaman Tidak Ditemukan (Bun)</h1>', {
                 status: 404,
                 headers: { 'Content-Type': 'text/html' },
             });
